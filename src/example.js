1d23d2b97de70f4fb152970af65e98943df4d0f3
Lê Quang Phương <quangphuongle1992@gmail.com>
2017-09-26 23:34:17 +0700

    Search with local datas
    - Initial component with local datas
    - Open match result with text input search
    - Example css for demo

--- src/example.js
+++ src/example.js
@@ -2,20 +2,52 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+import './example.css';
+
 import ReactJsSearchBox from './reactjs-search-box'
 
 export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            dataList: [{
+                id: 1,
+                name: 'Item 1'
+            }, {
+                id: 2,
+                name: 'Item 2'
+            }, {
+                id: 3,
+                name: 'Item 3'
+            }, {
+                id: 4,
+                name: 'Item 4'
+            }, {
+                id: 5,
+                name: 'Item 5'
+            }]
+        }
+    }
     render() {
         return (
-            <ReactJsSearchBox ref={ref => this.searchBox = ref} options={{
-                label: 'Test', placeHolder: 'test'
-            }} />
+            <ReactJsSearchBox ref={ref => this.searchBox = ref}
+                live={false}
+                datas={this.state.dataList}
+                api={{
+                    url: '',
+                    method: 'GET'
+                }}
+                action={{
+                    selectItem: this.selectItem.bind(this)
+                }} />
         );
     }
     componentDidMount() {
         this.searchBox.focus();
     }
-
+    selectItem(item) {
+        console.log(item);
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('container'))
--- src/reactjs-search-box.js
+++ src/reactjs-search-box.js
@@ -12,7 +12,9 @@ export default class SearchBox extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            valueInput: ''
+            valueInput: '',
+            openDropdown: false,
+            datas: props.datas
         }
     }
     render() {
@@ -20,30 +22,81 @@ export default class SearchBox extends Component {
         let state = this.state;
         let options = props.options;
         return (
-            <div className={DEFAULT.className}>
+            <div className={ClassNames(DEFAULT.className, { 'open': state.openDropdown })}>
                 <span className={ClassNames(`${DEFAULT.className}_label`)}>{options.label}</span>
                 <div className={ClassNames(`${DEFAULT.className}_content`)}>
                     <input ref={ref => this.inputSearch = ref} type="text"
                         className={ClassNames(`${DEFAULT.className}_input`)}
                         placeholder={options.placeHolder} value={state.valueInput}
                         onChange={this.onChange.bind(this)} />
+                    <div className={ClassNames(`${DEFAULT.className}_dropdown`)}>
+                        {props.live ? this.__liveMode(props.api) : this.__localMode(state.datas)}
+                    </div>
                 </div>
-            </div>
+            </div >
         );
+    }
+    componentDidMount() {
+        // document.addEventListener('click', this.__eventListener, false);
+    }
+    componentWillUnmount() {
+        // document.removeEventListener('click', this.__eventListener, false);
     }
     onChange(event) {
         let value = event.target.value;
-        this.setState({ valueInput: value });
+        let isEmpty = value === '';
+        let filterDatas = this.props.datas.filter(data => data.name.toUpperCase().indexOf(value.toUpperCase()) !== -1)
+        this.setState({ valueInput: value, openDropdown: !isEmpty, datas: isEmpty ? this.props.datas : filterDatas });
     }
     focus() {
         this.inputSearch.focus();
+    }
+    __localMode(datas) {
+        return this.__genDropdown(datas);
+    }
+    __liveMode(api) {
+
+    }
+    __genDropdown(datas) {
+        let ul = (li) => <ul>{li}</ul>;
+        let li = datas.map(data => {
+            return <li key={data.id}
+                className={ClassNames(`${DEFAULT.className}_item`,
+                    { 'selected': true }, { 'disable': true },
+                    { 'hidden': true })}
+                onClick={this.__selectItem.bind(data)}>{data.name}</li>
+        });
+        return ul(li);
+    }
+    __selectItem(item) {
+        // @TODO: do something if want check item before call props function
+        this.props.action.selectItem(item);
+    }
+    __eventListener() {
+
     }
 }
 
 SearchBox.propTypes = {
     options: PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        placeHolder: PropTypes.string.isRequired,
+        label: PropTypes.string,
+        placeHolder: PropTypes.string,
+    }),
+    live: PropTypes.bool,
+    datas: PropTypes.arrayOf(function (propValue, key, componentName, location, propFullName) {
+        if (typeof propValue[key].id !== 'number' || typeof propValue[key].name !== 'string') {
+            return new Error(
+                'Invalid prop `' + propFullName + '` supplied to' +
+                ' `' + componentName + '`. Validation failed.'
+            );
+        }
+    }),
+    api: PropTypes.shape({
+        url: PropTypes.string,
+        method: PropTypes.oneOf(['GET', 'POST']),
+    }),
+    action: PropTypes.shape({
+        selectItem: PropTypes.func.isRequired,
     })
 }
 
@@ -51,5 +104,14 @@ SearchBox.defaultProps = {
     options: {
         label: '',
         placeHolder: 'Search'
+    },
+    live: false,
+    datas: [{ id: 0, name: 'Default' }],
+    api: {
+        url: false,
+        method: 'GET'
+    },
+    action: {
+        selectItem: () => { }
     }
 }
